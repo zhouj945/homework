b4eea3ee9b5fa66fc61151385b8b516b5ef0c81e
zhoujian11 <zhoujian11@xdf.cn>
2020-08-03 14:26:51 +0800

    ..

--- task-03/generator-pages/generators/app/index.js
+++ task-03/generator-pages/generators/app/index.js
@@ -3,9 +3,9 @@ const Generator = require('yeoman-generator')
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts)
-    this.argument("init", { type: Boolean, desc: "舒适化", default: true })
-    this.option("ts");
-    this.fileExt = this.options.ts ? ".ts" : "js"
+    this.argument('init', { type: Boolean, desc: '舒适化', default: true })
+    this.option('ts')
+    this.fileExt = this.options.ts ? '.ts' : 'js'
   }
 
   methods() {
@@ -22,44 +22,43 @@ module.exports = class extends Generator {
         type: 'input',
         name: 'name',
         message: 'Please input you project name',
-        default: this.appname
-      }
+        default: this.appname,
+      },
     ])
-    this.pwdPath = '/' + this.anwsers.name + '/'
     this.log(this.contextRoot)
     // ———————利用 .yo-rc.json 保存 读取 配置—————————
-    await this.config.set({name: this.anwsers.name})
-    this.log(this.config.get("name"))
+    await this.config.set({ name: this.anwsers.name })
+    this.log(this.config.get('name'))
   }
 
   writing() {
+    // 利用输入判断 是否添加 plopfile 等等附加功能包
     // 可利用 node api 获取模版文件夹下的所有模版文件 再遍历
-    this.fs.copyTpl(
-      this.templatePath('index.html'),
-      this.destinationPath('public/index.html'),
-      { name: this.anwsers.name }
-    )
+    this.log('destinationPath', this.destinationPath())
+    this.fs.copyTpl(this.templatePath('index.html'), this.destinationPath('public/index.html'), {
+      name: this.anwsers.name,
+    })
 
     const pkgJson = {
       devDependencies: {
-        eslint: '^3.15.0'
+        eslint: '^3.15.0',
       },
       dependencies: {
-        react: '^16.2.0'
-      }
-    };
+        react: '^16.2.0',
+      },
+    }
 
     // if (this.options.ts) {
     //   this.npmInstall(['typescript'], { 'save-dev': true });
     // }
     // Extend or create package.json file in destination path
-    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
   }
 
   // install() {
   //   this.npmInstall();
   // }
   end() {
-    this.log("finish")
+    this.log('finish')
   }
 }
